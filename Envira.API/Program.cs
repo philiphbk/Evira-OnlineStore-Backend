e21bd28c08fe24276be7c913a0b7de7acf9653bd
Damilola Fajorin <philipfajorin@gmail.com>
2022-10-08 22:26:09 +0100

    AddedChanges

--- Envira.API/Program.cs
+++ Envira.API/Program.cs
@@ -1,6 +1,46 @@
+using Evira.Data.DbContext;
+using Evira.Data.Mappings;
+using Evira.Data.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+Log.Logger = new LoggerConfiguration()
+       .ReadFrom.Configuration(configuration)
+       .CreateLogger();
+
+try
+{
+    Log.Information("Hello, {Name}!", Environment.UserName);
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "The application failed to start correctly.");
+
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+builder.Host.UseSerilog();
+
+
+builder.Services.AddDbContext<EviraDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddAutoMapper(typeof(Mapping));
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();  
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
